2b402ecb4b9ce20ce815764462fbf90eb140df24
Janusz Janowski <januszjanowski@outlook.com>
2022-05-31 22:02:41 +0200

    Ważne zmiany v3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/calc', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def calc():
     html = """
     <h2> Kalkulator: </h2>
